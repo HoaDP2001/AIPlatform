1d7097f0ee881431cc286f316968151fa76619d9
HoaDP2001 <135573801+HoaDP2001@users.noreply.github.com>
2024-03-21 09:49:38 +0700

    9:49 PM

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -48,7 +48,6 @@ const ImagePage = () => {
       setImages([]);
 
       const response = await axios.post("/api/image", values);
-
       const urls = response.data.map((image: { url: string }) => image.url);
 
       setImages(urls);
